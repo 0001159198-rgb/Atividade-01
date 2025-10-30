33766358f73ce949af86107f0e1bfcd014333fd4
0001159198-rgb <0001159198@senaimgaluno.com.br>
2025-10-30 18:35:43 -0300

    Create TabuadaInvertida.java

---
+++ src/TabuadaInvertida.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TabuadaInvertida {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número para ver sua tabuada invertida: ");
+        int n = sc.nextInt();
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+
+        sc.close();
+    }
+}
